ca0f9e3e6f4833c7b6fc5397df20d41537571be8
suzyben48-dotcom <suzyben48@gmail.com>
2026-05-08 17:07:15 +0800

    更新 sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,24 @@
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
+const CACHE_NAME = 'lockscreen-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// 安装时缓存文件
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// 拦截请求，使其在无网络时也能打开
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
